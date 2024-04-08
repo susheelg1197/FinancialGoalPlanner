996e35470929840cb4b3f6ae22f80f425588ef8b
Shreya Deexit <shreyadeexit@Shreyas-MacBook-Pro.local>
2024-04-08 17:14:13 -0400

    enhanced goal creation UI

--- frontend/app/components/Header/index.js
+++ frontend/app/components/Header/index.js
@@ -12,12 +12,16 @@ import messages from './messages';
 
 function Header() {
   return (
+    
     <div style={{ backgroundColor: '#f0f0f0' }}>
       <div style={{ backgroundColor: '#333', color: 'white', padding: '10px' }} className="topnav">
           <a style={{ padding: '14px 16px', textDecoration: 'none', color:'white' }} className="active" href="#home">Home</a>
           <a style={{ padding: '14px 16px', textDecoration: 'none', color:'white' }} href="#news">Visualise</a>
           <a style={{ padding: '14px 16px', textDecoration: 'none', color:'white' }} href="#about">Help</a>
       </div>
+      <a href="https://www.reactboilerplate.com/">
+        <Img src={Banner} alt="react-boilerplate - Logo" />
+      </a>
       <NavBar>
         {/* NavBar content */}
       </NavBar>
